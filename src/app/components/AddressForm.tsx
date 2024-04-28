14045e69f2819494b35d2f879bf7cd16f4c187a0
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-04-29 05:25:35 +0700

    update website

--- src/app/components/AddressForm.tsx
+++ src/app/components/AddressForm.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddressForm: React.FC = () => {
     return (
         <div className="bg-gray-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full">
-            <h2 className="text-xl font-bold text-white mb-4">Enter your address</h2>
+            <h2 className="text-xl font-thin text-white mb-4">Enter your address</h2>
             <div className="mb-4">
                 <input className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Recipient's Name" />
             </div>
@@ -23,7 +23,7 @@ const AddressForm: React.FC = () => {
                 <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="zipcode" type="text" placeholder="Zip code" />
             </div>
             <div className="flex items-center justify-center">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-thin py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                     Confirm address
                 </button>
             </div>
--- src/app/components/PaymentSummary.tsx
+++ src/app/components/PaymentSummary.tsx
@@ -7,19 +7,22 @@ const PaymentSummary = ({ basketItems = [], onCheckout }) => {
 
     return (
         <div className="bg-gray-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <h2 className="text-xl font-bold mb-4">Payment</h2>
+            <h2 className="text-xl font-thin mb-4">Payment</h2>
             {basketItems.map(item => (
                 <div key={item.id} className="flex items-center justify-between mb-4">
+                    <div> 
                     <Image src="/background/sniperbg.jpeg" alt={item.name} width={100} height={100} />
+                    <span className="text-lg">{item.product_name}x</span>
+                    </div>
                     <span className="text-lg">{item.quantity}x</span>
                     <span className="text-lg">{item.price}</span>
                 </div>
             ))}
             <div className="flex items-center justify-between">
-                <span className="text-lg font-bold">Total price</span>
-                <span className="text-lg font-bold">{total}</span>
+                <span className="text-lg font-thin">Total price</span>
+                <span className="text-lg font-thin">{total}</span>
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mt-4"
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-thin py-2 px-4 rounded w-full mt-4"
             onClick={onCheckout}>
                 Check out
             </button>
