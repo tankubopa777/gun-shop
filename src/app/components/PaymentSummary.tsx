38b28729fb10d90d994353b66603a7734cc8a05a
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-04-29 20:32:01 +0700

    update

--- src/app/components/PaymentSummary.tsx
+++ src/app/components/PaymentSummary.tsx
@@ -5,17 +5,19 @@ import Image from 'next/image';
 const PaymentSummary = ({ basketItems = [], onCheckout }) => {
     const total = basketItems.reduce((sum, item) => sum + (item.price * item.quantity), 0);
 
+    // console.log(basketItems)
+
     return (
         <div className="bg-gray-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 className="text-xl font-thin mb-4">Payment</h2>
             {basketItems.map(item => (
-                <div key={item.id} className="flex items-center justify-between mb-4">
-                    <div> 
+                <div key={item.id} className="grid grid-cols-5 items-center justify-between mb-4">
+                   
                     <Image src="/background/sniperbg.jpeg" alt={item.name} width={100} height={100} />
-                    <span className="text-lg">{item.product_name}x</span>
-                    </div>
-                    <span className="text-lg">{item.quantity}x</span>
-                    <span className="text-lg">{item.price}</span>
+                    <span className="text-lg items-center col-span-2">{item.name}</span>
+                    
+                    <span className="text-lg items-end flex justify-end">{item.quantity}x</span>
+                    <span className="text-lg flex justify-center">{item.price}</span>
                 </div>
             ))}
             <div className="flex items-center justify-between">
